80588cb293fde6c44f2adabecd99b3a0e6cf28de
unknown <design.dokaza@gmail.com>
2024-06-30 18:46:46 +0200

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Travel",
+  description: "Travel App for Camping",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="scroll-smooth">
+      <body>
+      <NavBar />
+      <main className="relative overflow-hidden">
+        {children}
+      </main>
+      <Footer />
+      </body>
+    </html>
+  );
+}
